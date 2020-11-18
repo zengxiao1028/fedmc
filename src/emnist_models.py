fix sparsity computation change pruning staring round to 50

src/emnist_models.py
@@ -19,7 +19,7 @@ def create_model(only_digits=True, prune=True):
               tf.keras.layers.Dense(10 if only_digits else 62, activation=tf.nn.softmax)
               ]
     if prune:
-        layers = [prune_utils.PrunableLayer(l, pruning_sched.ConstantSparsity(0.5, 3)) for l in layers]
+        layers = [prune_utils.PrunableLayer(l, pruning_sched.ConstantSparsity(0.5, 50)) for l in layers]
     model = tf.keras.models.Sequential(layers)
     return model
 

src/training_loop.py
@@ -110,7 +110,7 @@ def _compute_numpy_l2_difference(model, previous_model):
 
 def _comopute_sparse_rate(model):
   flat = np.hstack([a.flatten() for a in model.prune_masks])
-  return np.mean(flat)
+  return 1 - np.mean(flat)
 
 
 def run(iterative_process: tff.templates.IterativeProcess,