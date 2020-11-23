903effe844c3341bc482a3951f44bfe3ed0be60d
xiaozeng <zengxiao1028@gmail.com>
2020-11-23 10:08:41 -0500

    update running script

--- run_original.sh
+++ run_original.sh
@@ -3,14 +3,21 @@
 export PYTHONPATH="${PYTHONPATH}:./federated"
 
 
-python federated/optimization/main/federated_trainer.py --task=emnist_cr \
+python federated/optimization/main/federated_trainer.py \
+                            --task=emnist_cr \
                             --total_rounds=1500 \
                             --client_optimizer=sgd \
-                            --client_learning_rate=0.1 \
+                            --client_learning_rate=0.0032 \
+                            --client_lr_schedule=exp_decay \
+                            --client_lr_decay_steps=500 \
+                            --client_lr_decay_rate=0.1 \
+                            --client_lr_staircase=true \
                             --client_batch_size=20 \
-                            --server_optimizer=sgd \
-                            --server_learning_rate=1.0 \
-                            --clients_per_round=50 \
+                            --clients_per_round=10 \
                             --client_epochs_per_round=1 \
-                            --experiment_name=emnist_fedavg \
+                            --server_optimizer=adam \
+                            --server_learning_rate=0.01 \
+                            --server_adam_beta_1=0.9 \
+                            --server_adam_beta_2=0.99 \
+                            --experiment_name=emnist_fedavg_official_hp5 \
                             --root_output_dir=./result
--- run_prune.sh
+++ run_prune.sh
@@ -4,16 +4,24 @@ export PYTHONPATH="${PYTHONPATH}:./federated"
 
 python federated_trainer.py --task=emnist_cr \
                             --total_rounds=1500 \
-                            --client_optimizer=adam \
-                            --client_learning_rate=0.01 \
+                            --client_optimizer=sgd \
+                            --client_learning_rate=0.0032 \
+                            --client_lr_schedule=exp_decay \
+                            --client_lr_decay_steps=500 \
+                            --client_lr_decay_rate=0.1 \
+                            --client_lr_staircase=true \
                             --client_batch_size=20 \
-                            --server_optimizer=sgd \
-                            --server_learning_rate=1.0 \
-                            --clients_per_round=50 \
+                            --clients_per_round=10 \
                             --client_epochs_per_round=1 \
+                            --server_optimizer=adam \
+                            --server_learning_rate=0.01 \
+                            --server_adam_beta_1=0.9 \
+                            --server_adam_beta_2=0.99 \
                             --rounds_per_checkpoint=100 \
-                            --experiment_name=emnist_fedavg_adam2 \
-                            --enable_prune=false \
-                            --root_output_dir=./result \
+                            --experiment_name=prune_emnist_fedavg_spsty80 \
+                            --enable_prune=true \
+                            --final_sparsity=0.8 \
+                            --root_output_dir=./result
+
 
 #sudo shutdown +1
